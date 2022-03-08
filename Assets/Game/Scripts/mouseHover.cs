f146805db31e3cf1b1abb2b6cba6f3a6cdbf0d07
DianaBoltersdorf <dboltersdorf@gmx.net>
2022-03-08 15:47:03 +0100

    Version 1

---
+++ Assets/Game/Scripts/mouseHover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseHover : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 1000f) && hit.rigidbody != null)
+        {
+            hit.rigidbody.AddForce(new Vector3(0, 1f, 0), ForceMode.Impulse);
+        }
+    }
+}
